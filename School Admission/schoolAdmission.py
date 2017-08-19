30eca335ced3010c587e19e57f40029895565463
Siddharth Pratap Singh <siddharthpratap92@gmail.com>
2017-08-18 22:51:43 -0400

    Update schoolAdmission.py

--- School Admission/schoolAdmission.py
+++ School Admission/schoolAdmission.py
@@ -6,11 +6,32 @@ def sigmoid(x):
 
 n_records,n_features=features_train.shape
 #weight_list
-w=np.random.normal(scale=1/n_features**.5, size=n_features)
+weights=np.random.normal(scale=1/n_features**.5, size=n_features)
 
 #hyper-parameters
 iteration=1000
 learning_rate=0.5
 
 for i in range(iteration):
+    delta_weight=np.zeros(n_features)
+    for x,y in zip(features_train,labels_train):
+        output=sigmoid(np.dot(weights,x))
+        #calculate the error y-y-hat
+        error=y-output
+        '''
+        calculate the weight step i.e learning_rate*delta*xi
+        delta=error*d(output)/d(wi)i.e error*d(sigmoid(x))/d(wi)
+        '''
+        #calculate error term
+        err_term=error*output*(1-output)
+
+        #calculate increase in weight with each feature
+        delta_weight+= err_term*x
+    weights+= learning_rate*delta_weight/n_features
+
+#calculate accuracy on the test data
+test_output=sigmoid(weights,features_test)
+predictions=test_output>0.5
+accuracy=accuracy_score(labels_test,predictions)
+print("Accuracy: {:.3f}".format(accuracy))
     
